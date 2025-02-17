926f8d3aa9a66b0264bfbcd6109b4e14ddf7d17d
Vadim Sadokhov <v.sadokhov@vk.team>
2025-02-17 23:43:07 +0300

    logic improvement

--- Sources/Scanner.swift
+++ Sources/Scanner.swift
@@ -1,92 +1,65 @@
-enum ScanError: Error {
-    case parsingFailed(line: Int, column: Int)
+fileprivate let START_ONE_LINE_COMMENT = "//"
+
+fileprivate func parseLine(_ lineIndex : Int, _ line : String) throws -> [Lexeme] {
+    var lexemes : [Lexeme] = []
+    var startIndex = line.startIndex
+    for columnIndex in line.indices {
+        if line[columnIndex].isWhitespace
+            || Lexeme.Token.Predefined(rawValue: String(line[columnIndex...columnIndex])) != nil {
+            let candidate = String(line[startIndex..<columnIndex])
+            if !candidate.isEmpty {
+                let leftIndex = line.distance(from: line.startIndex, to: startIndex)   
+                let rightIndex = line.distance(from: line.startIndex, to: columnIndex) 
+                let token: Lexeme.Token =
+                    if let predefined = Lexeme.Token.Predefined(rawValue: candidate) {
+                        .predefined(predefined)
+                    } else if candidate.first?.isLetter == true,
+                            candidate.allSatisfy(\.isLetter || \.isNumber) {
+                        .identifier(candidate)
+                    } else if let value = Int(candidate) {
+                        .integer(value)
+                    } else if let value = Double(candidate) {
+                        .real(value)
+                    } else {
+                        throw ScanError.parsingFailed(line: lineIndex, column: rightIndex)
+                    }
+
+                lexemes += [
+                        Lexeme(
+                        token: token,
+                        span: Lexeme.Span(line: lineIndex, range: leftIndex...rightIndex - 1)
+                    )
+                ]
+            }
+            if let singleCharPredefined = Lexeme.Token.Predefined(rawValue: String(line[columnIndex...columnIndex])) {
+                let index = line.distance(from: line.startIndex, to: columnIndex)
+                lexemes += [
+                    Lexeme(
+                        token: .predefined(singleCharPredefined),
+                        span: Lexeme.Span(line: lineIndex, range: index...index)
+                    )
+                ]
+            }    
+            startIndex = line.index(after: columnIndex)
+        }
+    }
+    return lexemes
+
 }
 
 func scan(_ source: String) throws -> [Lexeme] {
-    try source
-        .split(whereSeparator: \.isNewline)
-        .enumerated()
-        .flatMap { (lineIndex, line) in
-            var lexemes = [] as [Lexeme]
-            var startIndex = line.startIndex
-            for columnIndex in line.indices {
-                if let singleCharPredefined = Lexeme.Token.Predefined(rawValue: String(line[columnIndex...columnIndex])) {
-                    if startIndex != columnIndex {
-                        let candidate = String(line[startIndex..<columnIndex])
-                        let token: Lexeme.Token =
-                            if let predefined = Lexeme.Token.Predefined(rawValue: candidate) {
-                                .predefined(predefined)
-                            } else if candidate.first?.isLetter == true,
-                                      candidate.allSatisfy(\.isLetter || \.isNumber) {
-                                .identifier(candidate)
-                            } else if let value = Int(candidate) {
-                                .integer(value)
-                            } else { // Add Real
-                                throw ScanError.parsingFailed(line: lineIndex, column: columnIndex)
-                            }
-
-                        if case .predefined(.comment) = token {
-                            return lexemes
-                        }
-
-                        lexemes += [
-                            Lexeme(
-                                token: token,
-                                span: Lexeme.Span(line: lineIndex, range: startIndex...columnIndex - 1)
-                            )
-                        ]
-                    }
-                    if singleCharPredefined != .whitespace {
-                        lexemes += [
-                            Lexeme(
-                                token: .predefined(singleCharPredefined),
-                                span: Lexeme.Span(line: lineIndex, range: columnIndex...columnIndex)
-                            )
-                        ]
-                    }
-                    startIndex = line.index(after: columnIndex)
-                }
-            }
-
-            return lexemes
-        }
-}
-
-struct Lexeme {
-    var token: Token
-    var span: Span
-}
-
-extension Lexeme {
-    enum Token {
-        case identifier(String)
-        case real(Double)
-        case integer(Int)
-        case predefined(Predefined)
-
-        enum Predefined: String {
-            case null
-            case `true`
-            case `false`
-            case comment = "//"
-            case whitespace = " "
-            case parenthesisLeft = "("
-            case parenthesisRight = ")"
-            case quote
-            case quoteSign = "'"
-            case setq
-            case `func`
-            case lambda
-            case prog
-            case cond
-            case `while`
-            case `return`
-            case `break`
-        }
+    return try source
+    .split(whereSeparator: \.isNewline)
+    .enumerated()
+    .map{(index, line : String.SubSequence) in // start line from 1
+        (index + 1, line)
     }
-
-    struct Span {
-        var line: Int
-        var range: ClosedRange<Int>
+    .map{(index, line : String.SubSequence) in // remove texts after // 
+        (index, line.split(separator: START_ONE_LINE_COMMENT, omittingEmptySubsequences: false).first!)
     }
+    .filter{(_, line : Substring.SubSequence) in !line.isEmpty}  // delete empty string
+    .map{(index, line : Substring.SubSequence) in // add imaginary whitespace to simplify parseLine
+        (index, line + " ")
+    }
+    .flatMap(parseLine)
 }
