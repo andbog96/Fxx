d730f52c1b017db2319980fe60ddbed296a017bc
Andrey Bogdanov <andr.bogdanov@vk.team>
2025-04-26 10:05:46 +0300

    Simplify AST

--- Sources/AST.swift
+++ Sources/AST.swift
@@ -2,13 +2,9 @@ import NonEmpty
 
 typealias AST = AbstractSyntaxTree
 
-struct AbstractSyntaxTree {
-    var nodes: NonEmptyArray<Node>
-
-    indirect enum Node {
-        case keyword(Token.Keyword)
-        case literal(Token.Literal)
-        case identifier(Token.Identifier)
-        case list(NonEmptyArray<Node>)
-    }
+indirect enum AbstractSyntaxTree {
+    case keyword(Token.Keyword)
+    case literal(Token.Literal)
+    case identifier(Token.Identifier)
+    case list(NonEmptyArray<AbstractSyntaxTree>)
 }
